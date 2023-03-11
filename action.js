5e3d660d97f00cfa587cf9bf11fdf8f2dadd68fc
theseus-diagnostics-ui <127564755+theseus-diagnostics-ui@users.noreply.github.com>
2023-03-10 20:30:05 -0600

    Update action.js

--- action.js
+++ action.js
@@ -1,16 +1,18 @@
 /* -- Glow effect -- */
 
 const blob = document.getElementById("blob");
-
+let frame = null;
 window.onpointermove = event => { 
   const { clientX, clientY } = event;
   
-  blob.animate({
-    left: `${clientX}px`,
-    top: `${clientY}px`
-  }, { duration: 3000, fill: "forwards" });
+  frame = requestAnimationFrame(() => {
+    cancelAnimationFrame(frame);
+    blob.animate({
+      left: `${clientX}px`,
+      top: `${clientY}px`
+    }, { duration: 3000, fill: "forwards" });
+  })
 }
-
 /* -- Text effect -- */
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -21,14 +23,14 @@ const screens = document.querySelectorAll(".screen");
 screens.forEach(function(screen){
   screen.onmouseenter = event => {  
     let name = screen.querySelector(".name");
-    name.dataset.iteration=0;
+    name.dataset.iteration = 0;
     clearInterval(interval);
 
     interval = setInterval(() => {
       name.innerText = name.innerText
         .split("")
         .map((letter, index) => {
-          if(index < parseInt(name.dataset.iteration)) {
+          if((index*3) < parseInt(name.dataset.iteration)) {
             return name.dataset.value[index];
           }
 
@@ -36,15 +38,14 @@ screens.forEach(function(screen){
         })
         .join("");
 
-      if(parseInt(name.dataset.iteration) >= name.dataset.value.length){ 
+      if(parseInt(name.dataset.iteration) >= name.dataset.value.length * 3){ 
         clearInterval(interval);
       }
 
       name.dataset.iteration = parseInt(name.dataset.iteration) + 1;
     }, 30);
   }
-}
-               )
+})
 
 const locked = document.querySelector(".locked")
 
@@ -54,8 +55,17 @@ locked.onclick = function(){
   locked.classList.add("shake");  
 }
 
-const examine = document.querySelector(".examine")
+const examine = document.querySelector(".examine");
+// const examineClickContent = examine.querySelector(".examine-click-content");
+magnifyingGlassListener = function(event){
+  mag.classList.remove("fade-out");
+  mag.classList.add("hidden");
+  mag.removeEventListener("animationend", magnifyingGlassListener);
+//  examineClickContent.classList.add("examine-click-content-grow");
+}
 
 examine.onclick = function(){
-  
+  let mag = examine.querySelector(".magnifying-glass");
+  mag.classList.add("fade-out");
+  mag.addEventListener("animationend", magnifyingGlassListener);
 }
